9d644be3e4fa28d3899c071503628d9da81a8f31
Dmitry Fry <im@dmitryfry.com>
2019-10-24 15:14:02 +0300

    add info to log

--- app/controllers/unfollow_controller.rb
+++ app/controllers/unfollow_controller.rb
@@ -7,27 +7,32 @@ class UnfollowController < ApplicationController
   def unfollow(browser)
 
     AutogramController.new.authentication(browser)
-
+    logger.info "AutogramController.new.authentication(browser)"
     @logins.each do |login|
       browser.goto "instagram.com/#{login.name}/"
       sleep(rand(10..20))
-
+      logger.info(login.name)
       if browser.text.include?("Sorry, this page isn't available.")
+        logger.info("Sorry, this page isn't available.")
         login.used = true
         login.save
       else
         if browser.button(class: FOLLOW).exists? || browser.button(class: FOLLOW_PRIVATE_PAGE).exists?
+          logger.info("FOLLOW_PRIVATE_PAGE exists")
           login.used = true
           login.save
         elsif browser.button(class: REQUESTED).exists?
+          logger.info("REQUESTED exists")
           login.used = true
           login.save
         elsif browser.button(class: FOLLOWING).exists?
+          logger.info("FOLLOWING exists")
           begin
             browser.button(class: FOLLOWING).click
           rescue
           end
           if browser.button(class: UNFOLLOWING).exists?
+            logger.info("UNFOLLOWING exists")
             sleep(rand(2..7))
             begin
               browser.button(class: UNFOLLOWING).click
